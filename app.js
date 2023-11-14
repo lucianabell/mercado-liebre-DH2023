4bd288f3ee7b789869a789726c560b24f4296bf6
lucianabell <lucianabellgarcia@gmail.com>
2023-11-13 21:07:23 -0300

    actualizo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(port,() => console.log (`Servidor corriendo en el puerto ${port}`));
 
 app.use(express.static('public'));
 
-app.get('/home', (req, res)=>{
+app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
